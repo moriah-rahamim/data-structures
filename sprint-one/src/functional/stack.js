6745aaec7817c1e72b0e7670e366297310be7148
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 11:42:20 -0800

    Implemented rest of functional stack requirements

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,15 +5,31 @@ var Stack = function() {
   var storage = {};
   var count = 0;
 
-  // Implement the methods below
+  // Adds a new item onto the stack
   someInstance.push = function(value) {
+    // Add the new item onto the stack
+    storage[count] = value;
+    // Increment the count
     count++;
   };
 
+  // Takes the newest-added item off of the stack and returns that item
   someInstance.pop = function() {
-    if (count > 0) count--;
+    if (count > 0) { // If there are values in the stack
+      // take the newest value off, hold onto it
+      let value = storage[count - 1];
+      delete storage[count - 1];
+      // decrememt the count
+      count--;
+      // return the popped item
+      return value;
+    } else {
+      // Otherwise do nothing
+      return undefined;
+    }
   };
 
+  // Tells us how many items are currently in the stack
   someInstance.size = function() {
     return count;
   };

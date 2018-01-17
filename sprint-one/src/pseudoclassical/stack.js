022bfc5e1466841521e5591a6605c33d12657599
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 15:55:11 -0800

    implemented all requirements for test cases in pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,4 @@
-// Spec: Use pseudoclassical classes to construct a queue object
+// Spec: Use pseudoclassical classes to construct a stack object
 // Justification: To create new stacks whose methods are found via
 //   fallthrough lookups on a single object, taking advantage of
 //   pseudoclassical-style performance enhancements and very short,
@@ -10,21 +10,35 @@ var Stack = function() {
   // implied: this = Object.create(Stack.prototype);
 
   // add properties for count and storage
+  this.count = 0;
+  this.storage = {};
 
-  // implied: return this
+  // implied: retur_n this
 };
 
 // push method: push new item onto the stack
-Stack.prototype.push = function() {
-
+Stack.prototype.push = function(item) {
+  // add item to top of stack, increment count
+  this.storage[this.count] = item;
+  this.count++;
 };
 
 // pop method: pop item off top of the stack, return it
 Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    // remove item from top of stack, decrement count, return the item
+    var item = this.storage[this.count - 1];
+    delete this.storage[this.count - 1];
 
+    this.count--;
+
+    return item;
+  } else {
+    return undefined;
+  }
 };
 
 // size method: return size of the current stack
 Stack.prototype.size = function() {
-
+  return this.count;
 };

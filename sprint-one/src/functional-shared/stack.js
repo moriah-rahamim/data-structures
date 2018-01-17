b33b103037db9d3511ef07e6c4f2f1855dd3583b
Trevor Old <trevor.old@outlook.com>
2018-01-17 12:25:34 -0800

    Beginning scaffolding of functional shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function() {
+
+  },
+  pop: function() {
+
+  },
+  size: function() {
+
+  }  
+};
 
 
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};

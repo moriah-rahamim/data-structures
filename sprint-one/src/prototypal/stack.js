093cb1b5e665d01fe40785793c15e3839f7d37f6
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 14:52:58 -0800

    created some structure and description in comments for prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
+// Use prototypal classes to create a stack object.
+// Inputs: none | Outputs: stack object.
+// Behavior: create a new stack object that properly accesses
+//   fallthrough methods for push, pop, and size
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Add a new item onto a stack
+  push: function(item) {
+
+  },
+
+  // Remove the top item from the stack, and return it
+  pop: function() {
+
+  },
+
+  // Return the size of the stack
+  size: function() {
+
+  }
+};
 
 

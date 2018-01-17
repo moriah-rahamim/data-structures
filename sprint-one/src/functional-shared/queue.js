108c0d1efada5f9e907b5fbd69a69caca56c825c
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 12:36:08 -0800

    Implemented rest of requirements for functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -12,12 +12,24 @@ var Queue = function() {
 
 var queueMethods = {
   enqueue: function(item) {
+    // add item to back of queue
+    this.storage[this.startPosition + this.count] = item;
+    // increment the count
     this.count++;
   },
 
   dequeue: function() {
     if (this.count > 0) {
+      // take off the front item and hold onto its value
+      let item = this.storage[this.startPosition];
+      delete this.storage[this.startPosition];
+      // decrement the count and move the start position over
       this.count--;
+      this.startPosition++;
+      // return the dequeue'd item
+      return item;
+    } else {
+      return undefined;
     }
   },
 

a38cf421817b03cdce6fb63ee9170b936ba2e7e2
Trevor Old <trevor.old@outlook.com>
2018-01-17 15:00:10 -0800

    Functional-shared stack tests passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,27 +1,27 @@
 var Stack = function() {
   var newStack = {};
-
-  extend(newStack, stackMethods);
   
+  newStack.storage = {};
+  newStack.count = 0;
 
+  _.extend(newStack, methods);
+  
+  return newStack; 
 };
 
-var stackMethods = {};
-
-Stack.stackMethods.push = function () {
-
-};
-
-Stack.stackMethods.push = function () {
-
-};
-
-Stack.stackMethods.size = function () {
-
-};
-
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
-  }
+var methods = {
+  push: function (val) {
+    this.count++;
+    this.storage[this.count] = val;
+    this[this.storage] = this.storage[this.count];
+  },
+  pop: function() {
+    var val = this.storage[this.count];
+    delete this[this.storage[this.count]];
+    this.count --;
+    return val;
+  },
+  size: function() {
+    return this.count < 0 ? 0 : this.count;
+  } 
 };

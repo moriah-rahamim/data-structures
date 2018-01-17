d005eb33a5b9a2d46de7e13b1e5b2547d2e5ec88
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 11:45:56 -0800

    Implemented requirements for functional queue tests 1-5

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,20 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var count = 0;
 
   someInstance.enqueue = function(value) {
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+      count--;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;

0758e9170178e1f0b758585a43703d91c551ceda
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 15:04:45 -0800

    implemented remaining test requirements for prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -18,13 +18,21 @@ var Stack = function() {
 var stackMethods = {
   // Add a new item onto a stack
   push: function(item) {
+    // add the new item to the top and increment the count
+    this.storage[this.count] = item;
     this.count++;
   },
 
   // Remove the top item from the stack, and return it
   pop: function() {
-    if (this.count > 0) {
+    if (this.count > 0) { // if there is at least one item on the stack
+      // pop off the top item, decrement count, and return the popped item
+      let item = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+
       this.count--;
+
+      return item;
     }
   },
 

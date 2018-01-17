2b15b2c3948eb7689de7d80d4429b86d14587a7f
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 14:58:11 -0800

    Created structure for the constructor function to utilize stackMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,7 +3,16 @@
 // Behavior: create a new stack object that properly accesses
 //   fallthrough methods for push, pop, and size
 var Stack = function() {
-  
+  // stackMethods is a prototype for the stack
+  // (i.e. failed property lookups are delegated to stackMethods)
+  var someInstance = Object.create(stackMethods);
+
+  // keep track of number of items in the stack
+  someInstance.count = 0;
+  // holds whatever is in the stack
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var stackMethods = {

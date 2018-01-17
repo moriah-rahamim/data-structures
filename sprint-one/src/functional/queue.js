393dd6dcacdeedc592403ecc1aebe164c1eeb495
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 11:57:06 -0800

    Implemented all remaining test requirements for functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,13 +5,30 @@ var Queue = function() {
   var storage = {};
   var count = 0;
 
+  // Start position tells us what number currently represents the front of the queue
+  var startPosition = 0;
+
   someInstance.enqueue = function(value) {
+    // add the newest item onto the back of the queue
+    storage[startPosition + count] = value;
+    // increment the count
     count++;
   };
 
   someInstance.dequeue = function() {
     if (count > 0) {
+      // remove the oldest item from the front of the queue
+      let value = storage[startPosition];
+      delete storage[startPosition];
+      // update the start position for the queue
+      startPosition++;
+      // decrement the count
       count--;
+      // return the item that we removed from the queue
+      return value;
+    } else {
+      // otherwise, do nothing
+      return undefined;
     }
   };
 

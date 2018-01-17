5276f04087f350c7d6f7a92418e4a5d36b0f679c
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 15:15:50 -0800

    Added structure and high-level pseudocode for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
+// Spec: Use prototypal classes to construct a queue object
+// Justification: To create new queues whose methods are found via
+//   fallthrough lookups on a single separate object, with no need
+//   for copying each property to the queue object
+// Behavior: Create a new queue object that properly accesses fallthrough
+//   methods for enqueue, dequeue, and size
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create someInstance with queueMethods as a prototype
+  // (i.e. failed property lookups are delegated to queueMethods)
+
+  // Create variables for count, start position, and storage for the queue
+
+  // return the newly-created queue
+
 };
 
-var queueMethods = {};
+// Object to hold the methods used by Queue objects
+// This will be the prototype object for the Queue objects
+var queueMethods = {
+  // add a new item to the back of the queue
+  enqueue: function(item) {
+
+  },
+
+  // remove oldest item from the front of the queue, and return it
+  dequeue: function() {
+
+  },
+
+  // return the current size of the queue
+  size: function() {
+
+  }
+};
 
 

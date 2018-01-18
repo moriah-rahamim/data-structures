a8d3c70c110f252532d3839833542978a24443cd
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 16:04:34 -0800

    implemented all requirements for test for pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -10,22 +10,41 @@ var Queue = function() {
   // implied: this = Object.create(Queue.prototype)
 
   // define properties for count, startPosition, and storage
+  this.count = 0;
+  this.startPosition = 0;
+  this.storage = {};
 
   // implied: retur_n this
 };
 
 // enqueue method: add new item to back of queue
-Queue.prototype.enqueue = function() {
-
+Queue.prototype.enqueue = function(item) {
+  // add item to end of queue, increment count
+  this.storage[this.startPosition + this.count] = item;
+  this.count++;
 };
 
 // dequeue method: remove item from front of queue, return it
 Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    // remove item from front of queue and hold onto it
+    var item = this.storage[this.startPosition];
+    delete this.storage[this.startPosition];
+
+    // increment startPosition, decrememt count
+    this.startPosition++;
+    this.count--;
+
+    // return the item that we removed
+    return item;
+  } else {
+    return undefined;
+  }
 
 };
 
 // size method: return the size of the current queue
 Queue.prototype.size = function() {
-
+  return this.count;
 };
 

38249cb75922dfe68f30c1cdc5d82768c4e91484
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 16:00:03 -0800

    added structure and high-level pseudocode and comments for pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
+// Spec: Use pseudoclassical classes to construct a queue object
+// Justification: To create new queues whose methods are found via
+//   fallthrough lookups on a single object, taking advantage of
+//   pseudoclassical-style performance enhancements and very short,
+//   clean code structure
+// Behavior: Create a new queue object that properly accesses fallthrough
+//   methods for enqueue, dequeue, and size from prototype property as the
+//   fallback object
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // implied: this = Object.create(Queue.prototype)
+
+  // define properties for count, startPosition, and storage
+
+  // implied: retur_n this
 };
 
+// enqueue method: add new item to back of queue
+Queue.prototype.enqueue = function() {
+
+};
+
+// dequeue method: remove item from front of queue, return it
+Queue.prototype.dequeue = function() {
+
+};
+
+// size method: return the size of the current queue
+Queue.prototype.size = function() {
+
+};
 

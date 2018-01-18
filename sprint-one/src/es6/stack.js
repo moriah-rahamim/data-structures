5080b3ed144b7ac25e366af80b54d4c40fbd39e6
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-17 16:15:17 -0800

    implemented all test requirements for es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -8,12 +8,23 @@ class Stack {
 
   // add new item to top of stack
   push(item) {
-
+    // add item to top of stack, increment the count
+    this.storage[this.count] = item;
+    this.count++;
   }
 
   // remove item from top of stack, return it
   pop() {
-
+    if (this.count > 0) {
+      // remove item from top of stack, hold onto it
+      let item = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      // decrement count, return the item
+      this.count--;
+      return item;
+    } else {
+      return undefined;
+    }
   }
 
   // return size of current stack

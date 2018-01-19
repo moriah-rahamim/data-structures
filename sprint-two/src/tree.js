ec19b65db93e62fea878ea16e0b903566577d61a
moriah-rahamim <moriahfaye@gmail.com>
2018-01-19 10:27:05 -0800

    Implemented base tests and functionality for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create new tree with value
+  //add newtree as a child of this
+  var newTree = Tree(value);
+  this.children.push(newTree);
+  
+  
 };
 
 treeMethods.contains = function(target) {
+  //first check if the current tree matches target
+  if (this.value === target) {
+    return true;
+  }
+  //create a var isFound = false
+  var isFound = false;
+  for (var i = 0; i < this.children.length; i++) {
+    //if not : recursively call contains on each of its children
+    isFound = isFound || this.children[i].contains(target);
+  }
+  //and return result of that, if one contains true, return true;
+  return isFound;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   addChild = O(1);
+   contains = O(n);
  */

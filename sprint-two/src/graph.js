ce34b3e55243b5bb48ee9a8b150d8ebcfa74e56a
moriah-rahamim <moriahfaye@gmail.com>
2018-01-19 15:41:28 -0800

    Implemented forEachNode functionality, added nodes property to graph class

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,6 +3,7 @@
 // Instantiate a new graph
 var Graph = function() {
   
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
@@ -12,14 +13,14 @@ Graph.prototype.addNode = function(value) {
   node.value = value;
   node.edges = {};
   //add that object as a property of this with key = value
-  this[value] = node;
+  this.nodes[value] = node;
   
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(value) {
   //check if that value has a key:value in the graph  
-  if (this[value]) {    
+  if (this.nodes[value]) {
     return true;
   } else {
     return false;
@@ -31,17 +32,17 @@ Graph.prototype.removeNode = function(value) {
   //go through every edge in this nodes edges
   //for each edge
   //  remove this edge from the other's nodes edges object
-  for (var key in this[value].edges) {
-    delete this[key].edges[value];
+  for (var key in this.nodes[value].edges) {
+    delete this.nodes[key].edges[value];
   }
   // remove the node from the graph
-  delete this[value];
+  delete this.nodes[value];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   // check fromNode's edges property to see if it has an edge to toNode
-  return this[fromNode].edges[toNode] !== undefined;
+  return this.nodes[fromNode].edges[toNode] !== undefined;
   // should not need to do the reverse because it would yield same result
 };
 
@@ -50,8 +51,8 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
   // check if graph contains both the fromNode and the toNode
   if (this.contains(fromNode) && this.contains(toNode)) {
     //set edge reference between both nodes(bi-directional)
-    this[fromNode].edges[this[toNode].value] = this[toNode].value;
-    this[toNode].edges[this[fromNode].value] = this[fromNode].value;
+    this.nodes[fromNode].edges[this.nodes[toNode].value] = this.nodes[toNode].value;
+    this.nodes[toNode].edges[this.nodes[fromNode].value] = this.nodes[fromNode].value;
 
     // return true if successful
     return true;
@@ -66,19 +67,31 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
   // make sure both nodes exist in the graph
   if (this.contains(fromNode) && this.contains(toNode)) {
     // delete the edge from the fromNode edges
-    delete this[fromNode].edges[toNode];
+    delete this.nodes[fromNode].edges[toNode];
     // delete it from the toNode edges
-    delete this[toNode].edges[fromNode];
+    delete this.nodes[toNode].edges[fromNode];
     // note: delete will fail gracefully and return false if the edge didn't exist
   }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
+  var nodes = this.nodes;
+  for (var node in nodes) {
+    cb(node);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addNode: O(1);
+ contains: O(1);
+ removeNode: O(n) with respect to the number of edges the node has
+ hasEdge: O(1);
+ addEdge: O(1);
+ removeEdge: O(1);
+ forEachNode: O(n);
  */
 
 

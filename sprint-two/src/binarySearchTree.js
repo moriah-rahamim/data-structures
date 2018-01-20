9364a32dd5dcfd2dab9cab9e8e14b5f657ec6cf3
moriah-rahamim <moriahfaye@gmail.com>
2018-01-20 10:24:52 -0800

    implemented .depthFirstLog for test 4, and added time complexity in comments

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -57,11 +57,32 @@ var bstMethods = {
   },
 
   depthFirstLog: function(callback) {
+    // execute callback on this node
+    callback(this.value);
 
+    // check if node has left child
+    if (this.left !== null) {
+      // if so, recurse on left child
+      this.left.depthFirstLog(callback);
+    }
+    // check if node has right child
+    if (this.right !== null) {
+      // if so, recurse on right child
+      this.right.depthFirstLog(callback);
+    }
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Assuming a self-balancing tree, it would be:
+  * insert: O(log n)
+  * contains: O(log n)
+  * depthFirstLog: O(n)
+ * Assuming a non-self-balancing tree, the actual worst case is:
+  * insert: O(n)
+  * contains: O(n)
+  * depthFirstLog: O(n)
+ * this is not self-balancing so technically all operations here are O(n)
  */
  

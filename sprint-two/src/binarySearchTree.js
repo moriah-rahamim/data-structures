d97476c4d03e7aac9afd7ecdf67f148ce872de67
moriah-rahamim <moriahfaye@gmail.com>
2018-01-20 09:40:31 -0800

    created structure for functional-shared class for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,30 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  _.extend(tree, treeMethods);
+
+  return tree;
 };
 
+var treeMethods = {
+
+  insert: function(value) {
+
+  },
+
+  contains: function(value) {
+
+  },
+
+  depthFirstLog: function(callback) {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 

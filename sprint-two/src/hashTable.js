32d6195f14b7fe123204c67901175070a7b3a22e
moriah-rahamim <moriahfaye@gmail.com>
2018-01-19 19:33:01 -0800

    Added time complexity comments

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -58,6 +58,9 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
-
-
+  .insert O(n) with respect to the length of they key assuming high entropy and good distribution
+  .retrieve O(n) with respect to the length of they key assuming high entropy and good distribution
+  .remove O(n) with respect to the length of they key assuming high entropy and good distribution
+  note: in the worst case if all keys hash to the same bucket everthing is O(n) 
+        in respect to the number items in the stored in the hashtable
+*/

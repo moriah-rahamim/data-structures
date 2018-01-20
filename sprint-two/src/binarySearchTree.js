79f325e658fa68f18af7b603b1db6ee44db6c0a1
moriah-rahamim <moriahfaye@gmail.com>
2018-01-20 09:57:33 -0800

    implemented .insert for binary search tree. tests 1-2 passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,15 +4,35 @@ var BinarySearchTree = function(value) {
   tree.left = null;
   tree.right = null;
 
-  _.extend(tree, treeMethods);
+  _.extend(tree, bstMethods);
 
   return tree;
 };
 
-var treeMethods = {
+var bstMethods = {
 
   insert: function(value) {
+    // do nothing if value is equal to this node's value
+    if (this.value !== value) {
+      var side;
+      if (value > this.value) {
+        // if value is greater than the current node's value
+        side = 'right';
+      } else if (value < this.value) {
+        // if value is less than the current node's value
+        side = 'left';
+      }
 
+      // check if this node has a child on that side
+      if (this[side] === null) {
+        // if no - add this new value as a child on that side
+        var newNode = BinarySearchTree(value);
+        this[side] = newNode;
+      } else {
+        // if yes - recurse on that side's child
+        this[side].insert(value);
+      }
+    }
   },
 
   contains: function(value) {
